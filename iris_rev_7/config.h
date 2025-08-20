60bd1eeb9514b20d89641ea978a8a8cfe0e870be
critterwilson <43965806+critterwilson@users.noreply.github.com>
2025-08-20 10:05:27 -0600

    Update config.h

--- iris_rev_7/config.h
+++ iris_rev_7/config.h
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #pragma once
 /* key matrix size */
 // Rows are doubled-up
@@ -10,6 +26,11 @@
 #define MATRIX_ROW_PINS_RIGHT { B1, F0, F5, B4, B5 }
 #define MATRIX_COL_PINS_RIGHT { D4, D6, D7, C7, F1, F4 }
 #define SPLIT_HAND_PIN D5
+
+// #define ENCODERS_PAD_A { B3, F6 }
+// #define ENCODERS_PAD_B { B2, F7 }
+#define ENCODERS_PAD_A_RIGHT { B3, F6 }
+#define ENCODERS_PAD_B_RIGHT { B2, F7 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -29,7 +50,6 @@
 /* ws2812 RGB LED */
 #define RGB_DI_PIN E6
 #ifdef RGB_DI_PIN
-
 /*#define RGBLED_NUM 68*/
 #define RGBLED_SPLIT { 34, 34 }
 #define RGBLIGHT_HUE_STEP 8
